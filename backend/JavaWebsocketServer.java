fad1966b3cec7bc0f35bade211b2d72fc15c606c
jokercoplay <1061567173@qq.com>
2017-09-15 12:37:41 +0800

    basic mongo

--- backend/JavaWebsocketServer.java
+++ backend/JavaWebsocketServer.java
@@ -1,5 +1,3 @@
-package websocketDemo;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
--- backend/MongodbTest.php
+++ backend/MongodbTest.php
@@ -1,3 +1,21 @@
-<?php
+<?php 
     $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-    echo $manager;
+
+    $bulk = new MongoDB\Driver\BulkWrite;
+    $document = ['name' => 'hai'];
+    
+    $bulk->insert($document);
+
+    $bulk->update(
+        ['name' => 'dnoy'],
+        ['$set' => ['name' => 'dony.dang']]
+    );
+
+    $bulk->delete(['name' => 'hai']);
+
+    $manager->executeBulkWrite('test.user', $bulk);
+
+
+    $filter = ['name' => 'hai'];
+    $query = new MongoDB\Driver\Query($filter);
+    $res = $manager->executeQuery('test.user', $query);
